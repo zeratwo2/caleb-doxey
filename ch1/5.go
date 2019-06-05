1ca1c7c8c0110c220d415ba58c9ebad2ac83abf8
zeratwo2 <zeratwo@gmail.com>
2019-06-05 10:37:18 +0300

    zadania 1,2,3

---
+++ ch1/5.go
@@ -0,0 +1,8 @@
+//Измените написанную программу так, чтобы вместо Hello World она выводила Hello, my name is вместе с вашем именем.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello my name is vitya")
+}
